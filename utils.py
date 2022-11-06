5305c401bac3b393df849c89861b233004f0f3af
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2022-11-06 11:17:49 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,7 +382,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://short.io/api'
+    url = f'https://hmtdmovies.tk/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
