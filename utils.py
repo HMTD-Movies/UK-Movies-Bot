51f09fecbf2b00d806191cd31cabb17c3fa5d6f4
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2022-11-12 18:29:55 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -382,7 +382,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://mdiskpro.xyz/api'
+    url = f'https://dropLink.co/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
