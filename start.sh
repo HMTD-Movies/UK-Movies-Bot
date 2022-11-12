81c63c4509d915317110e49e00ec2738dd86e835
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2022-11-12 19:09:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
+  git clone https://github.com/HMTD-Links/UK-Auto-Filter-Bot /Eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Eva
