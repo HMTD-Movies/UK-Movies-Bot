5ff8c4c99ff4044b20801b5116ccaf558b10e4cd
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2022-11-13 18:00:54 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -54,7 +54,7 @@ async def save_group(bot, message):
                     except:
                         pass
                 temp.MELCOW['welcome'] = await message.reply_video(
-                video="https://telegra.ph/file/03691465baa774e46506d.mp4",                                               
+                video="https://telegra.ph/file/11d612c9f9a61c19427b0.mp4",                                               
                                                  caption=f'<b>Hello 👋🏻, {u.mention} Welcome to Our Group\nUK Movies Group\nWe are Providing Tamil and Tamil Dubbed Movies. More Languages Coming Soon. I Can Support Upto 4GB File. You Can Get added Files GDrive Links and 4GB above Links also. You Can Get GDrive Links in www.HMTDMovies.tk. Keep me Join to Our Official Channel to Receive Bot & Movies Updates in @UK_Studios_Official.</b>',
                                                  reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('🚫 Group Rules 🚫', url='http://t.me/MissRose_bot?start=rules_-1001650088903') ] ] )
                 )
