448d2cebcfdf51164c2e701f41242f4d1b83d21f
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2022-11-28 20:41:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -178,7 +178,7 @@ async def advantage_spoll_choker(bot, query):
                                        
                                        ]]
                 ),
-                parse_mode='html'
+                parse_mode=enums.ParseMode.HTML
 )
 
             await asyncio.sleep(10)
