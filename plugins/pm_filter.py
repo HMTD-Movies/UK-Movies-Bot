533b4d63aefda1f849e898d85a49c4d6e513ae58
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2022-11-28 17:33:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -724,6 +724,8 @@ async def auto_filter(client, msg, spoll=False):
         InlineKeyboardButton('😎 Group 😎', url='https://t.me/HMTD_Discussion_Group'),
         InlineKeyboardButton('☺️ Share ☺️', url='https://t.me/share/url?url=%2A%2A%F0%9F%98%B1+UK+Movies+Zone%C2%B2%C2%B7%E2%81%B0+%F0%9F%98%B1+%0D%0A+%0D%0AWhatever+you+might+ask%2C+you+will+get..+%E2%9D%A4%EF%B8%8F+%0D%0A+%0D%0A%F0%9F%91%87+Join%0D%0A+https%3A%2F%2Ft.me%2FUK_Movies_Zone'),
         InlineKeyboardButton('📡 Channel 📡', url='https://t.me/UK_Studios_Official')
+        ]
+    )
     imdb = await get_poster(search, file=(files[0]).file_name) if settings["imdb"] else None
     TEMPLATE = settings['template']
     if imdb:
