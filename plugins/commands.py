1b61ac85e079b85ef5cf50f7c808ac782627c086
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2022-12-08 15:00:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -285,7 +285,7 @@ async def channel_info(bot, message):
 
 @Client.on_message(filters.command('logs') & filters.user(ADMINS))
 async def log_file(bot, message):
-    ""<b>Send Log File 📂</b>"""
+    ""Send Log File 📂"""
     try:
         await message.reply_document('TelegramBot.log')
     except Exception as e:
