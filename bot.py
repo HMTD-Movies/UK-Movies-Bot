605778615dba5774e621d00ef60992242c9a4b23
HMTD-Movies <111636041+HMTD-Movies@users.noreply.github.com>
2022-12-23 10:54:00 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,8 +15,6 @@ from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
-from aiohttp import web
-from os import environ
 
 
 PORT = environ.get("PORT", "8080")
@@ -47,20 +45,6 @@ class Bot(Client):
         self.username = '@' + me.username
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, PORT).start()
-
-async def web_server():
-    web_app = web.Application(cilent_max_size=30000000)
-    web_app.add_routes(routes)
-    return web_app
-
-routes = web.RouteTableDef()
-@routes.get("/", allow_head=True)
-async def root_route_handler(request):
-    return web.json_response("testbot")
 
 
     async def stop(self, *args):
